4fc81c1ac62d3873eef4b19300ed0fc62c88c3ac
krykiet@github.com <chudzik.kd@gmail.com>
2022-10-18 15:00:01 +0200

    created index and controller. Closes #8

---
+++ pet-clinic-web/src/main/java/com/sfgpetclinic/controllers/indexController.java
@@ -0,0 +1,16 @@
+package com.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+
+    // Different request options
+    @RequestMapping({"","/","index","index.html"})
+    // Thymeleaf needs this to look for template called index.html
+    public String index() {
+
+        return "index";
+    }
+}
