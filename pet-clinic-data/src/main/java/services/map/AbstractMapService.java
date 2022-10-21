07f4f231ab47e5cab5fa46a5cd729ffa600befcb
krykiet@github.com <chudzik.kd@gmail.com>
2022-10-21 11:03:04 +0200

    implemented find next ID in AbstractMapService

--- pet-clinic-data/src/main/java/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/services/map/AbstractMapService.java
@@ -1,13 +1,12 @@
 package services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -17,8 +16,22 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
+    // Getting next ID
+    private Long getNextId() {
+        return Collections.max(map.keySet()) + 1;
+    }
+
     T save(ID id, T object) {
-        map.put(id, object);
+        if (object != null) {
+            // if object has no ID we set it by getNextId method
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+            // HashMap with key=ID, value=object
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
         return object;
     }
 
