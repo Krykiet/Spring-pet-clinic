bfac24e9547a3f3284a493a365088775a07919be
krykiet@github.com <chudzik.kd@gmail.com>
2022-10-21 10:43:02 +0200

    listed vets on vets/index, Closes #15

--- pet-clinic-web/src/main/java/com/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/sfgpetclinic/controllers/VetController.java
@@ -1,12 +1,25 @@
 package com.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import services.VetService;
 
 @Controller
 public class VetController {
+
+    private final VetService vetService;
+
+    // @Autowired - automatically by Spring
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets/index", "/vets", "/vets/index.html" })
-    public String listVets(){
+    public String listVets(Model model){
+
+        model.addAttribute("vetsSet", vetService.findAll());
+
         return "vets/index";
     }
 }
