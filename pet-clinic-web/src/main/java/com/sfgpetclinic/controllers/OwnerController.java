7417b3dcc54fead81c5f0f9e7b2a9c20684a3db1
krykiet@github.com <chudzik.kd@gmail.com>
2022-10-21 10:19:44 +0200

    listed owners on page, injected OwnerService to OwnerController and created model for passing data to page

--- pet-clinic-web/src/main/java/com/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/sfgpetclinic/controllers/OwnerController.java
@@ -1,15 +1,28 @@
 package com.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import services.OwnerService;
 
 // Double request mapping
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    // injecting OwnerService dependency by constructor
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/", "/index", "/index.html"})
-    public String listOwners(){
+    // bring a Spring Model - interface for passing objects to UI
+    public String listOwners(Model model){
+        // attributeName and value inside the model
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
