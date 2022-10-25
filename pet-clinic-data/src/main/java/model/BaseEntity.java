3395a08cf31b08afcacb9ff0feefdb0b7847454d
krykiet@github.com <chudzik.kd@gmail.com>
2022-10-25 13:15:35 +0200

    Converted Vets to JPA entities, Closes #37

--- pet-clinic-data/src/main/java/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/model/BaseEntity.java
@@ -9,6 +9,7 @@ import java.io.Serializable;
 @MappedSuperclass // we inherit from this, not map this to database
 public class BaseEntity implements Serializable {
 
+    //This is inherited by all classes, we do not need to set up this everywhere
     @Id // tells JPA this is the ID value
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment for MySQL
     private Long id;
