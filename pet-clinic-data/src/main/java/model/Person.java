da6e7c2dc15b890fff97cf9e9d21ca02abd3e8e1
krykiet@github.com <chudzik.kd@gmail.com>
2022-10-25 13:03:45 +0200

    Completed JPA mappings to Owner entities, Closes #36

--- pet-clinic-data/src/main/java/model/Person.java
+++ pet-clinic-data/src/main/java/model/Person.java
@@ -1,8 +1,15 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass // never working with this POJO, only extending
 public class Person extends BaseEntity {
 
+    @Column(name = "first_name") // hibernate does this automatically (snake_case)
     private String firstName;
+
+    @Column(name = "last_name") // hibernate does this automatically (snake_case)
     private String lastName;
 
     public String getFirstName() {
--- pet-clinic-data/src/main/java/model/Pet.java
+++ pet-clinic-data/src/main/java/model/Pet.java
@@ -1,12 +1,24 @@
 package model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne // many pets have same type, ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne // many pets can have same owner, ManyToOne
+    @JoinColumn(name = "owner_id") // this tells Hibernate / JPA how to do mapping
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public String getName() {
