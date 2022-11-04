b8b52f19337398985c029f988398f773766f9783
Krykiet <chudzik.kd@gmail.com>
2022-11-04 18:47:32 +0100

    Added broken ownerDetails view

--- pet-clinic-web/src/test/java/com/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -70,4 +70,17 @@ class OwnerControllerTest {
 
         verifyNoInteractions(ownerService);
     }
+
+    @Test
+    void displayOwner() throws Exception {
+
+        Owner mockOwner = Owner.builder().id(1L).build();
+        // Set-up Mockito
+        when(ownerService.findById(anyLong())).thenReturn(mockOwner);
+
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
+    }
 }
