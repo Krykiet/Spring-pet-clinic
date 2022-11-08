f97e764ab4a33ced49f44c0d75787e7cab9ecad2
Krykiet <chudzik.kd@gmail.com>
2022-11-08 18:04:28 +0100

    Added json endpoint, closes #68

--- pet-clinic-web/src/main/java/com/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.sfgpetclinic.controllers;
 
+import com.sfgpetclinic.model.Vet;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.sfgpetclinic.services.VetService;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -21,5 +26,11 @@ public class VetController {
         model.addAttribute("vetsSet", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    @ResponseBody // by default returns JSON
+    public Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
